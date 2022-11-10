4b8e18c472f47b87277387d778ece488cd4fc4ee
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-11-10 14:07:18 -0300

    fix: rotas

--- src/App.js
+++ src/App.js
@@ -41,11 +41,11 @@ function App() {
         <Route exact path="/meals/:id" component={ MealDetails } />
         <Route
           exact
-          path="/meals/:id-da-receita/in-progress"
+          path="/meals/:id/in-progress"
           component={ RecipeInProgress }
         />
         <Route
-          path="/drinks/:id-da-receita/in-progress"
+          path="/drinks/:id/in-progress"
           component={ RecipeInProgress }
         />
       </Switch>
